e2dac9cc52bc60cbe26378a32ca09aa9fafed845
sumitsrivastava069 <61084331+sumitsrivastava069@users.noreply.github.com>
2024-09-11 17:04:52 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -4,6 +4,6 @@ import streamlit as st
 st.title("Hello World App")
 
 if st.button("Get Greeting"):
-    response = httpx.get("http://backend:80")
+    response = httpx.get("http://172.30.109.56:8080")
     data = response.json()
     st.write(data["message"])
